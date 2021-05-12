a41dd30bacc94e5288b4c51199d243db5581fd98
Duncan Champney <DChampney@wareto.com>
2021-05-11 22:04:53 -0400

    Initial commit

---
+++ AnimatePaths/AnimatePathsView.swift
@@ -0,0 +1,91 @@
+//
+//  AnimatePathsView.swift
+//  AnimatePaths
+//
+//  Created by Duncan Champney on 5/10/21.
+//
+
+import UIKit
+
+/// A struct to define a path and the duration, pause, and timing function to use for an animation step.
+struct PathStep {
+    var path: CGPath
+    var stepDuration: Double?
+    var stepPause: Double?
+    var timingFunction: CAMediaTimingFunctionName?
+}
+
+/**
+ This class uses a `CAShapeLayer` as it's backing layer. It takes an array of `PathStep` objects, and initially installs the path for the first step as the layer's path.
+ If you call the `animate(repeats:)` function, it creates a `CAAnimationGroup` of animations that animate the path from the first to the last path in the paths array,
+ and back to the initial path.
+ */
+class AnimatePathsView: UIView {
+
+    var paths: [PathStep]? {
+        didSet {
+            guard let paths = paths,
+                  let firstPath = paths.first?.path,
+                  let layer = self.layer as? CAShapeLayer else { return }
+            layer.path = firstPath
+        }
+    }
+
+    public var defaultStepDuration: Double = 0.5
+    public var defaultStepPause: Double = 0.2
+
+    class override var layerClass: AnyClass {
+        return CAShapeLayer.self
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSetup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initSetup()
+    }
+
+    func initSetup() {
+        guard let layer = self.layer as? CAShapeLayer else { return }
+        layer.fillColor = UIColor.cyan.cgColor
+        layer.strokeColor = UIColor.black.cgColor
+        layer.lineWidth = 1
+        layer.path = nil
+    }
+
+    /**
+     This function animates the array of paths in the `paths` array from the first to last, and then animates bak to the first element in the array. The 'stepDuration`,`'stepPause`, and `timingFunction` properties of each `PathStep in the array determine the duration of the animation from the previous path to the path contained in that PathStep objects. The animation uses default values for any properties of a PathStep that are nil.
+     - Parameter repeats: If true, the animation repeats indefinitely.
+     */
+    func animate(repeats: Bool) {
+        var startTime = 0.0
+        guard let layer = self.layer as? CAShapeLayer,
+              let paths = paths,
+              paths.count > 1 else { return }
+        let animationGroup = CAAnimationGroup()
+        animationGroup.animations = [CABasicAnimation]()
+        for index in paths.indices {
+            let path = paths[index]
+            let nextindex = (index + 1) % paths.count
+            let pathAnimation = CABasicAnimation(keyPath: "path")
+            let stepDuration = paths[nextindex].stepDuration ?? defaultStepDuration
+            pathAnimation.duration = stepDuration
+            pathAnimation.beginTime = startTime
+            pathAnimation.timingFunction = CAMediaTimingFunction(name: paths[1].timingFunction ?? CAMediaTimingFunctionName.easeInEaseOut)
+            pathAnimation.fromValue = path.path
+            pathAnimation.isRemovedOnCompletion = false
+            pathAnimation.fillMode = CAMediaTimingFillMode.forwards
+            pathAnimation.toValue = paths[nextindex].path
+            startTime += stepDuration + (paths[nextindex].stepPause ?? defaultStepPause)
+            animationGroup.animations?.append(pathAnimation)
+        }
+        animationGroup.duration = startTime
+        if repeats {
+            animationGroup.repeatCount = .infinity
+        }
+        layer.add(animationGroup, forKey: nil)
+    }
+}
--- AnimatePaths/ViewController.swift
+++ AnimatePaths/ViewController.swift
@@ -8,12 +8,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pathsView: AnimatePathsView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidLayoutSubviews() {
+        setupAnimationPaths()
+    }
+
+    /**
+     Install an array of PathStep objects containing a pause button shape and a play button shape into our `pathsView` `AnimatePathsView`
+     The shapes are centered in the view.
+    */
+    func setupAnimationPaths() {
+
+        //-----------------------------
+        // Create a path that draws a pause symbol (two vertical bars) centered in the pathsView
+        let pausePath =  CGMutablePath()
+        let viewMiddleX = pathsView.bounds.midX
+        let viewMiddleY = pathsView.bounds.midY
+        let pauseBarSize = CGSize(width: 20, height: 90)
+        let leftRectOrigin = CGPoint(x: viewMiddleX - pauseBarSize.width * 3 / 2, y: viewMiddleY - pauseBarSize.height / 2 )
+        let leftBarRect = CGRect(origin: leftRectOrigin, size: pauseBarSize)
+        let rightBarRect = leftBarRect.offsetBy(dx: pauseBarSize.width * 2, dy: 0)
+        if true {
+            // Create the left rectangle of the pause symbol. We do this by moving to the top left corner, then adding lines clockwise
+            // for the remaining 3 points. Finally we call closeSubpath() to turn the rectangle into a closed path.
+            // We could also create the 2 rectangles using the CGMutablePath method `addRect(_:transform:)` and the result would be the same.
+            pausePath.move(to: leftRectOrigin)
+            pausePath.addLine(to: CGPoint(x: leftBarRect.maxX, y: leftBarRect.origin.y ))
+            pausePath.addLine(to: CGPoint(x: leftBarRect.maxX, y: leftBarRect.maxY ))
+            pausePath.addLine(to: CGPoint(x: leftBarRect.origin.x, y: leftBarRect.maxY ))
+            pausePath.closeSubpath()
+
+            // Create the right side rectangle of the pause symbol
+            pausePath.move(to: rightBarRect.origin)
+            pausePath.addLine(to: CGPoint(x: rightBarRect.maxX, y: rightBarRect.origin.y ))
+            pausePath.addLine(to: CGPoint(x: rightBarRect.maxX, y: rightBarRect.maxY ))
+            pausePath.addLine(to: CGPoint(x: rightBarRect.origin.x, y: rightBarRect.maxY ))
+            pausePath.closeSubpath()
+        } else {
+            // This code would have exactly the same result as the code above that draws the rectangle one line segment at a time.
+            pausePath.addRect(leftBarRect)
+            pausePath.addRect(rightBarRect)
+        }
+
+        //--------------------------
+        // Create a path that draws a play symbol triangle, but as 2 joined quadralaterals where the right quadralateral
+        // has 2 of it's points at the same position so it is drawn in the shape of a triangle.
+        let playPath =  CGMutablePath()
+        let playPathHeight: CGFloat = 80.0
+        let playPathWidth: CGFloat = 76.0
+
+        // Create the left quadralateral as a trapezoid
+        playPath.move(   to: CGPoint(x: viewMiddleX - playPathWidth / 2, y: viewMiddleY - playPathHeight / 2))
+        playPath.addLine(to: CGPoint(x: viewMiddleX, y: viewMiddleY - playPathHeight / 4 ))
+        playPath.addLine(to: CGPoint(x: viewMiddleX, y: viewMiddleY + playPathHeight / 4 ))
+        playPath.addLine(to: CGPoint(x: viewMiddleX - playPathWidth / 2, y: viewMiddleY + playPathHeight / 2 ))
+        playPath.closeSubpath()
+
+        // Create the right quadralateral with it's right 2 points together, so it draws as a triangle.
+        playPath.move(to: CGPoint(x: viewMiddleX, y: viewMiddleY - playPathHeight / 4 ))
+
+        //The right 2 points are the same, turning the quadralateral into a triangle
+        playPath.addLine(to: CGPoint(x: viewMiddleX + playPathWidth / 2, y: viewMiddleY))
+        playPath.addLine(to: CGPoint(x: viewMiddleX + playPathWidth / 2, y: viewMiddleY))
+
+        playPath.addLine(to: CGPoint(x: viewMiddleX, y: viewMiddleY + playPathHeight / 4))
+        playPath.closeSubpath()
+
+        pathsView.paths = [
+            PathStep(path: pausePath),
+            PathStep(path: playPath)
+        ]
+    }
+
+    @IBAction func handleAnimateButton(_ sender: Any) {
+        pathsView.animate(repeats: false)
+    }
 
 }
 
